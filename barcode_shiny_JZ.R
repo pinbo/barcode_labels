61ab41a85474b1cf350dda75fad40408143c7539
Junli Zhang <zhjl86@gmail.com>
2023-02-16 21:29:16 -0800

    add a new tube label template and text wrap

--- barcode_shiny_JZ.R
+++ barcode_shiny_JZ.R
@@ -1,5 +1,6 @@
 # 2022-10-25: v2: using R package barcodeLabel v0.2.0
 library(shiny)
+# library(grid)
 # devtools::install_github("pinbo/barcodeLabel")
 library(barcodeLabel)
 # UI
@@ -20,10 +21,23 @@ library(barcodeLabel)
          column(width = 2,
          radioButtons("sep", "Separator",
             choices = c(Comma = ",", Tab = "\t"),
-            selected = ",")),
+            selected = "\t")),
          column(width = 2,
          checkboxInput("header", "Header in file?", value=TRUE))
          ),
+        p(strong("Or input manually if all labels are the same")),
+        fluidRow(
+          column(width = 5,
+                 textInput("simpleText", "Text for all labels (use '\\n' for line break)", value = "abc\\n123")),
+          column(width = 3,
+                 numericInput("simpleTextRepeat", 
+                              "Number of labels", 
+                              value = 4, min = 1)),
+          column(width = 2,
+                 textInput("font_col", "Text color", value = "black"))
+
+        ),
+        actionButton("createDataset", "Create Dataset"),
          # Select variables to display ----
          uiOutput("select_column"),
          p(strong("3. Custom text (can add multiple times)")),
@@ -39,7 +53,8 @@ library(barcodeLabel)
          selectInput("type","Barcode Type", 
                       choices = list("Linear (1D)" = "linear",
                                      "QR Code" = "qr",
-                                     "Data Matrix" = "dm"),
+                                     "Data Matrix" = "dm",
+                                     "No barcode (only text)" = "null"),
                       multiple = FALSE)),
          column(width = 4,
          numericInput("font_size", 
@@ -59,7 +74,7 @@ library(barcodeLabel)
             selectInput(
               inputId = "label_type",
               label   = "Preset Label Type",
-              c('Avery 5967 (0.5" x 1.75")' = "avery5967", 'Avery 5960 (1.0" x 2.63")' = "avery5960")
+              c('Avery 5967 (0.5" x 1.75")' = "avery5967", 'Avery 5960 (1.0" x 2.63")' = "avery5960", 'Tough Spots 3/8in' = 'Tough-Spots-3/8in')
             )),
           column(width = 3,
            selectInput(
@@ -132,11 +147,29 @@ library(barcodeLabel)
          column(
            width = 3,
            radioButtons("showborder", 
-                        "Show border?", 
+                        "Show border for output?", 
                         choices = c(Yes = TRUE, No = FALSE),
                         selected = FALSE, inline = TRUE)
-         )
-         ), width = 8
+         ),
+        column(
+          width = 3,
+          radioButtons("border_type", 
+                       "Rectangle or circle border?", 
+                       choices = c("rectangle", "circle", "both"),
+                       selected = "rectangle", inline = TRUE)
+        )
+         ), 
+        fluidRow(
+          column(width = 4,
+                 numericInput("ERow", 
+                              "Label print from Row", 
+                              value = 1, min=1, max=100)),
+          column(width = 4,
+                 numericInput("ECol", 
+                              "Label print from Column", 
+                              value = 1, min=1, max=100))
+        ),
+        width = 8
       ),
       
       # Main panel for displaying outputs ----
@@ -181,30 +214,44 @@ library(barcodeLabel)
     # pdf making server side
     # check label file
     mydata = reactiveVal()
-    Labels_pdf<-reactive({
-      req(input$labels)
+    rawdata = reactiveVal()
+
+    observeEvent(input$labels,{
       df<-read.csv(input$labels$datapath, header=input$header, stringsAsFactors = FALSE, sep = input$sep)
+      rawdata(df)
       df$text2add = "" # add an column for text to add
       df$tmp = "" # store last step
       mydata(df)
-      df
     })
+    
+    observeEvent(input$createDataset, {
+      if(input$simpleText != "" & input$simpleTextRepeat > 0){
+        tt = gsub("\\\\n","\n",input$simpleText) # need this to get the correct line break
+        df = data.frame(label=rep(tt, input$simpleTextRepeat))
+        rawdata(df)
+        df$text2add = "" # add an column for text to add
+        df$tmp = "" # store last step
+        mydata(df)
+      }
+    })
+
     
     # select column to make labels
     # Dynamically generate UI input when data is uploaded ----
     output$select_column <- renderUI({
-      cc = ncol(Labels_pdf())
+      cc = ncol(rawdata())
       selectInput(inputId = "barcode_var", label = "2. Choose the variable for creating barcode", 
-                  choices = names(Labels_pdf())[-c(cc-1, cc)], multiple=FALSE)
+                  # choices = names(mydata())[-c(cc-1, cc)], multiple=FALSE) 
+                  choices = names(rawdata()), multiple=FALSE)
     })
     
     # add text
     output$add_text <- renderUI({
-      cc = ncol(Labels_pdf())
+      cc = ncol(mydata())
       fluidRow(
         column( width = 3, textInput("prefix",  "Text Prefix", value = "")  ),
         column( width = 3, selectInput( inputId = "variable", label   = "Variable to add",
-            choices = c(Choose = "", names(Labels_pdf())[-c(cc-1, cc)])   )
+            choices = c(Choose = "", names(mydata())[-c(cc-1, cc)])   )
         ),
         column(
           width = 3, selectInput("fontface", "Font face?", choices = c(plain=1, bold=2, italic=3, boldItalic=4))
@@ -270,10 +317,24 @@ library(barcodeLabel)
        updateNumericInput(session, "label_width", value = 2.63)
        updateNumericInput(session, "label_height", value = 1.0)
      }
+      else if (input$label_type == "Tough-Spots-3/8in"){
+        updateNumericInput(session, "numrow", value = 16)
+        updateNumericInput(session, "numcol", value = 12)
+        updateNumericInput(session, "page_width", value = 8.4375)
+        updateNumericInput(session, "page_height", value = 11)
+        updateNumericInput(session, "width_margin", value = 0.5625)
+        updateNumericInput(session, "height_margin", value = 0.58)
+        updateNumericInput(session, "label_width", value = 0.375)
+        updateNumericInput(session, "label_height", value = 0.375)
+        updateNumericInput(session, "font_size", value = 10)
+        updateRadioButtons(session, "text_align", selected = "center")
+        updateRadioButtons(session, "border_type", selected = "circle")
+        updateSelectInput(session, "type", selected = "null")
+      }
     }, ignoreInit = TRUE)
     # set text alignment and barcode height based on barcode type selection
     observeEvent(input$type, {
-      if (input$type == "linear"){
+      if (input$type == "linear" | input$type == "null"){
         updateNumericInput(session, "barcode_height", value = 0.5)
         updateNumericInput(session, "barcode_scale", value = 1)
         updateRadioButtons(session, "text_align", selected = "center")
@@ -285,7 +346,7 @@ library(barcodeLabel)
     
     # dataset for drawing
     tmp_label_list <-reactive({
-    req(input$labels)
+      req(mydata())
     # 1. create simple element layout on each label
     simple_label_layout(
       barcode_text=mydata()[,input$barcode_var],
@@ -295,7 +356,10 @@ library(barcodeLabel)
       barcode_on_top = input$barcode_on_top,
       barcode_height = input$barcode_height,
       barcode_type=input$type, font_size = input$font_size,
-      fontfamily = input$fontfamily, useMarkdown = T, ecl = as.integer(input$ecl), barcode_scale=input$barcode_scale)
+      fontfamily = input$fontfamily, useMarkdown = T, 
+      ecl = as.integer(input$ecl), 
+      barcode_scale=input$barcode_scale,
+      font_col = input$font_col)
     })
     
     # preview label file
@@ -308,7 +372,12 @@ library(barcodeLabel)
                      height = input$label_height,
                      units = "in", res=300, family = input$fontfamily
                      )
-      grid::grid.rect()
+      if (input$border_type == "rectangle") grid::grid.rect()
+      else if (input$border_type == "circle") grid::grid.circle(r=grid::unit(min(input$label_width, input$label_height)/2, "inches"))
+      else{# both
+        grid::grid.rect()
+        grid::grid.circle(r=grid::unit(min(input$label_width, input$label_height)/2, "inches"))
+      }
 
       if (length(vp_list) > 0){
         for (j in 1:length(vp_list)){
@@ -344,6 +413,7 @@ library(barcodeLabel)
         name = input$filename, # pdf output file name
         fontfamily = input$fontfamily, # "mono", "sans", "serif"
         showborder = input$showborder, # whether to show border of labels
+        border_type = input$border_type,
         vp_list = tmp_label_list()$vp_list,
         content_list = tmp_label_list()$content_list,
         numrow = input$numrow, 
@@ -355,7 +425,9 @@ library(barcodeLabel)
         label_width = input$label_width, 
         label_height = input$label_height, 
         text_align = input$text_align, # left or center
-        useMarkdown = T
+        useMarkdown = T,
+        ECols = input$ECol - 1,
+        ERows = input$ERow - 1
       )
       status<-"Done!"
       status
